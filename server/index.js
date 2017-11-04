ee87f1913f8f9e017886c9d319e3fed3a4a7da68
Manjhunath Ravi <manjhunath.kr@gmail.com>
2017-11-04 17:46:29 -0600

    server getting location- made some changes

--- server/index.js
+++ server/index.js
@@ -2,18 +2,14 @@ var express = require('express');
 var path = require('path');
 var port = 3000;
 var app = express();
-var bodyParser =  require('body-parser');
+var router = express.Router()
 
-// var connection = require('http').createServer(app);
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get('/events',function(req,res){
-  //console.log("Server side req: "+req.body.location)
   res.send(req.query);
-
-
 });
 
 app.listen(port, function(){
