93da5cc2612b65bb3eab1cba70579ddf779bd73f
Manjhunath Ravi <manjhunath.kr@gmail.com>
2017-11-14 19:42:18 -0700

    css with react-bootstrap

--- client/src/components/EventItem.jsx
+++ client/src/components/EventItem.jsx
@@ -1,18 +1,54 @@
 import React from 'react';
+import {Panel, Media, Button, Collapse, Well} from 'react-bootstrap';
 
-const EventItem = ({event}) => {
-  const date = event.start.local.split("T")[0];
-  const time = event.start.local.split("T")[1];
+class EventItem extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      date: this.props.event.start.local.split("T")[0],
+      time: this.props.event.start.local.split("T")[1]
+    }
+  }
+  
+  render() {
+    return (
 
-  return (
-    <div>
-      <img height="100" width="200" src= {event.logo.url} />
-      <h1><a href={event.url}>{event.name.text}</a></h1>
-      <p>{event.description.text}</p>
-      <h3>{date}</h3>
-      <h3>{time}</h3>
-    </div>
-  )
+      <Panel>
+        <Media onClick={() => {this.setState({open: !this.state.open})}}>
+
+          <Media.Left>
+            <img src={this.props.event.logo.url} height={64} width={128} alt="Event Image"/>
+          </Media.Left>
+
+          <Media.Body>
+            <Media.Heading><a href={this.props.event.url} target="_blank">{this.props.event.name.text}</a></Media.Heading>
+            <div>
+              Local Date: {this.state.date}
+            </div>
+
+            <div>
+              Local Time: {this.state.time}
+            </div>
+
+            <div>
+              {/* <Button bsStyle="small" onClick={() => {this.setState({open: !this.state.open})}}> Description </Button> */}
+                <Collapse in={this.state.open}>
+                  <div>
+                    <Well>  {this.props.event.description.text}  </Well>
+                  </div>  
+                </Collapse>
+            </div>
+          </Media.Body>
+
+        </Media>
+      </Panel>
+
+  
+    );
+
+  }
+  
 }
 
 export default EventItem;
