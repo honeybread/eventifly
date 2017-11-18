d3af7e40c926cef21f15de4ad6ca473e3421705b
Manjhunath Ravi <manjhunath.kr@gmail.com>
2017-11-18 16:39:29 -0700

    basic css working. Maps feature commented out

--- client/src/components/EventItem.jsx
+++ client/src/components/EventItem.jsx
@@ -1,20 +1,80 @@
 import React from 'react';
 import MapContainer from './Googlemaps.jsx';
+import {Panel, Media, Collapse, Well} from 'react-bootstrap';
 
-const EventItem = ({event}) => {
-  var details = event.details.eventbrite? event.details.eventbrite: event.details.yelp;
-  return (
-    <div>
-      <img height="150" width="150" src= {details.logoUrl} alt={"Event Image Not Available"}/>
-      <h1><a target="_blank" href={details.eventUrl}>{details.name}</a></h1>
-      <p>{details.description}</p>
-      <p>{event.startDate}</p>
-      <p>{event.startTime}</p>
-      <MapContainer  lat={event.lat} lng={event.long} name={details.name}/>
-    </div>
-  )
+class EventItem extends React.Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      open: false,
+      details: this.props.event.details.eventbrite? this.props.event.details.eventbrite: this.props.event.details.yelp
+    }
+  }
+
+  render() {
+    return (
+      <Panel>
+        <Media onClick={() => {this.setState({open: !this.state.open})}}>
+          <Media.Left>
+            <img height="150" width="150" src= {this.state.details.logoUrl} alt={"Event Image Not Available"}/>
+          </Media.Left>
+          <Media.Body>
+            <Media.Heading><a target="_blank" href={this.state.details.eventUrl}>{this.state.details.name}</a></Media.Heading>
+            
+            <p>{this.props.event.startDate}</p>
+            <p>{this.props.event.startTime}</p>
+            <div>
+              <Collapse in={this.state.open}>
+              <div>
+                <Well>  {this.state.details.description}
+                
+                {/* <Panel collapsible expanded={this.state.open}>
+                    <MapContainer lat={this.props.event.lat} lng={this.props.event.long} name={this.state.details.name}/>
+                </Panel> */}
+                </Well>
+              </div>  
+              </Collapse>
+            </div>
+  
+            
+          </Media.Body>
+          
+        </Media>
+      </Panel>
+    )
+  }
+  
 }
 
 export default EventItem;
 
+// Panel>
+// <Media onClick={() => {this.setState({open: !this.state.open})}}>
 
+//  <Media.Left>
+//     <img src={this.props.event.logo.url} height={64} width={128} alt=“Event Image”/>
+//   </Media.Left>
+
+//  <Media.Body>
+//     <Media.Heading><a href={this.props.event.url} target=“_blank”>{this.props.event.name.text}</a></Media.Heading>
+//     <div>
+//       Local Date: {this.state.date}
+//     </div>
+
+//    <div>
+//       Local Time: {this.state.time}
+//     </div>
+
+//    <div>
+//       {/* <Button bsStyle=“small” onClick={() => {this.setState({open: !this.state.open})}}> Description </Button> */}
+//         <Collapse in={this.state.open}>
+//           <div>
+//             <Well>  {this.props.event.description.text}  </Well>
+//           </div>  
+//        </Collapse>
+//     </div>
+//   </Media.Body>
+
+// </Media>
+// </Panel>
